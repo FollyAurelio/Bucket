36cd411a0fe1f8388b6415765dbf328efdb5c156
FollyAurelio <folly.aurelio@gmail.com>
2025-06-09 20:50:24 +0100

    Progess on python file

--- piece_table.py
+++ piece_table.py
@@ -1,22 +1,74 @@
 class Sequence:
 
-    def __init__(self):
+    def __init__(self, filepath):
         self.piece_table = PieceTable()
-        self.file_buffer = ""
         self.add_buffer = ""
+        try:
+            with open(filepath, "r") as rf:
+                self.file_buffer = rf.read()
+        except:
+            self.file_buffer = ""
+        f = Span(0,0,len(self.file_buffer))
+        f.next = self.piece_table.head.next
+        self.piece_table.head.next = f
+        f.prev = f.next.prev
+        f.next.prev = f
+        
     
-    def insert(self):
-        pass
+    def insert(self, index, text, seq_length):
+        new_span = Span(1, len(self.add_buffer), len(text))
+        old_span = SpanRange()
+        self.add_buffer += text
+        offset = 0
+        sptr = self.piece_table.head.next
+        while sptr:
+            if (offset <= index and index < offset + sptr.length):
+                if offset == index:
+                   #old_span.append(sptr.prev)
+                   #old_span.append(sptr)
+                   sptr.prev.next = new_span
+                   new_span.prev = sptr.prev
+                   new_span.next = sptr
+                   sptr.prev = new_span
+                else:
+                    pass
+                return 1
+            else:
+                offset += sptr.length
+                sptr = sptr.next
+        return 0
+
+        """ if offset == index:
+                    pass
+                else:
+                    pass"""
+            
+
+        
 
     def erase(self):
         pass
+
+    def __str__(self):
+        output = ""
+        sptr = self.piece_table.head.next
+        while sptr.next:
+            if not sptr.buffer:
+                output += self.file_buffer[sptr.start:sptr.start+sptr.length]
+            else:
+                output += self.add_buffer[sptr.start:sptr.start+sptr.length]
+            sptr = sptr.next
+        return output
+            
+
+
 
 
 class PieceTable:
     
     def __init__(self):
         self.head = Span(0,0,0)
-        self.tail = Span(0,0,0)
+        self.tail = Span(0,0,1)
         self.head.next = self.tail
         self.tail.prev = self.head
 
@@ -30,21 +82,29 @@ class Span:
         self.start = start
         self.length = length
 
-def print_piece_table(sequence):
-    sptr = sequence.piece_table.head.next
-    while sptr.next:
-        if not sptr.buffer:
-            print(sequence.file_buffer[sptr.start:sptr.start+sptr.length], end="")
+class SpanRange:
+
+    def __init__(self):
+        self.first = None
+        self.last = None
+        self.boundary = None
+        self.length = 0
+
+    def append(self, span):
+        if not self.first and not self.last and not self.boundary:
+            self.first = span
+            self.last = span
+            self.boundary = False
+        elif self.first == self.last and not self.boundary:
+            self.boundary = True
+            self.last = span
         else:
-            print(sequence.add_buffer[sptr.start:sptr.start+sptr.length], end="")
-        sptr = sptr.next
+            self.boundary = False
+            self.last = span
 
 
-s = Sequence()
-s.file_buffer = "The brown fox jumped over the lazy dog.\nThe end"
-f = Span(0,0,47)
-s.piece_table.head.next.prev = f
-s.piece_table.head.next = f
-f.prev = s.piece_table.head
-f.next = s.piece_table.tail
-print_piece_table(s)
+
+s = Sequence("test.txt")
+print(len(s.file_buffer))
+s.insert(45,"Or is it?",3)
+print(s)
